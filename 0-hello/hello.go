a1e456893c1604e16acc81cfa26c4f72e59b6171
guitarbien <guitarbien@gmail.com>
2020-01-25 11:18:36 +0800

    feat: add language parameter and pass Spanish

--- 0-hello/hello.go
+++ 0-hello/hello.go
@@ -4,14 +4,18 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
+	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
 	}
 
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("", ""))
 }
