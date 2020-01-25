f1ee3ab0f4227661d7a781e032c41569b23c4816
guitarbien <guitarbien@gmail.com>
2020-01-25 11:08:53 +0800

    refactor: test for with or without parameter

--- 0-hello/hello_test.go
+++ 0-hello/hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	assertCorrectMessage := func(t *testing.T, got string, want string) {
+		t.Helper()
 
-	if got != want {
-		t.Errorf("got '%q', want '%q'", got, want)
+		if got != want {
+			t.Errorf("got '%q', want '%q'", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
