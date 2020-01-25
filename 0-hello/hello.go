fc92d8d5666505f9e150df2fc33da0625000098a
guitarbien <guitarbien@gmail.com>
2020-01-25 11:36:46 +0800

    refactor: use private function & `named return value`

--- 0-hello/hello.go
+++ 0-hello/hello.go
@@ -13,16 +13,19 @@ func Hello(name string, language string) string {
 		name = "world"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case spanish:
 		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
 
 func main() {
