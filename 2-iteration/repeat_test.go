76c4862b7fa298f72145d7be90ec7ec1703bdc2f
guitarbien <guitarbien@gmail.com>
2020-01-26 16:06:29 +0800

    test: 可以指定重複的次數

--- 2-iteration/repeat_test.go
+++ 2-iteration/repeat_test.go
@@ -3,16 +3,30 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
-
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+	assertCorrectMessage := func(t *testing.T, expected string, repeated string) {
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
 	}
+
+	t.Run("repeat a character default with 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+
+		assertCorrectMessage(t, expected, repeated)
+	})
+
+	t.Run("repeat a character with 2 times", func(t *testing.T) {
+		repeated := Repeat("a", 2)
+		expected := "aa"
+
+		assertCorrectMessage(t, expected, repeated)
+	})
 }
+
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i <= b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
