7179e89b9e3db257993f8a5e6bb8a1cecbbe90b5
guitarbien <guitarbien@gmail.com>
2020-01-26 16:06:48 +0800

    feat: implementing

--- 2-iteration/repeat.go
+++ 2-iteration/repeat.go
@@ -1,9 +1,9 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, count int) string {
 	var repeated string
 
-	for i := 0; i < 5; i++ {
+	for i := 1; i <= count; i++ {
 		repeated += character
 	}
 
