56b98cdba8ad444ba1c85f10ff601f9cf2d6743f
guitarbien <guitarbien@gmail.com>
2020-01-27 20:03:35 +0800

    test: anonymous struct 加入 name 並用在 t.Run() 中，可透過 go test -run {test method}/{run name} 來指定執行，並在 Errorf() 透露出更精確方便 debug 的訊息

--- 4-struct-mehtod-interface/shape_test.go
+++ 4-struct-mehtod-interface/shape_test.go
@@ -14,19 +14,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct{
+		name string
 		shape Shape
 		want float64
 	}{
-		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
 	for _, test := range areaTests {
-		got := test.shape.Area()
+		t.Run(test.name, func(t *testing.T) {
+			got := test.shape.Area()
 
-		if got != test.want {
-			t.Errorf("got %.2f want %.2f", got, test.want)
-		}
+			if got != test.want {
+				t.Errorf("%#v got %.2f want %.2f", test.shape, got, test.want)
+			}
+		})
 	}
 }
