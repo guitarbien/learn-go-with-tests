22c9633ff068b68d0fe1151eb3889fb758c092ff
guitarbien <guitarbien@gmail.com>
2020-01-27 13:05:30 +0800

    test: 加入使用 slice 不指定長度的情境

--- 3-array-slice/sum_test.go
+++ 3-array-slice/sum_test.go
@@ -3,11 +3,23 @@ package array_slice
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
 
-	if want != got {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any number", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
