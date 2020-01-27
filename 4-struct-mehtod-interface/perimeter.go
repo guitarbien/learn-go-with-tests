92ef97b4cf2447a0a9a81832426e97d6bfd394d7
guitarbien <guitarbien@gmail.com>
2020-01-27 18:15:35 +0800

    feat: calculate area

--- 4-struct-mehtod-interface/perimeter.go
+++ 4-struct-mehtod-interface/perimeter.go
@@ -3,3 +3,7 @@ package struct_mehtod_interface
 func Perimeter(width, height float64) float64 {
 	return 2 * (width + height)
 }
+
+func Area(width, height float64) float64 {
+	return width * height
+}
