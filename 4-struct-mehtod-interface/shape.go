0769a02210c5d103419483d881d67aec38194d1d
guitarbien <guitarbien@gmail.com>
2020-01-27 19:45:39 +0800

    feat: 加入三角形 type 並實作 interface

--- 4-struct-mehtod-interface/shape.go
+++ 4-struct-mehtod-interface/shape.go
@@ -15,6 +15,10 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
 type Circle struct {
 	Radius float64
 }
@@ -23,6 +27,11 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
