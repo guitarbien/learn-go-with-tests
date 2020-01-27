c31f5baebd86496f8b600ec22d60a85c72a3bb74
guitarbien <guitarbien@gmail.com>
2020-01-27 18:59:46 +0800

    feat: 將 Area() 改為 method
    A method is a function with a receiver.

--- 4-struct-mehtod-interface/shape.go
+++ 4-struct-mehtod-interface/shape.go
@@ -5,14 +5,18 @@ type Rectangle struct {
 	Height float64
 }
 
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
 type Circle struct {
 	Radius float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+func (c Circle) Area() float64 {
+	return 0
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
