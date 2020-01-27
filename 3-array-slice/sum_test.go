d647b1d7410302d2e62f0806078e63f89201e97e
guitarbien <guitarbien@gmail.com>
2020-01-27 13:06:35 +0800

    test: 刪除重複情境的測試

--- 3-array-slice/sum_test.go
+++ 3-array-slice/sum_test.go
@@ -3,16 +3,6 @@ package array_slice
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-		got := Sum(numbers)
-		want := 15
-
-		if want != got {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
-	})
-
 	t.Run("collection of any number", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 		got := Sum(numbers)
