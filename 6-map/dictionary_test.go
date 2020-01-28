fccaa07e61f12a98e273d548128c8ba4fe7f4902
guitarbien <guitarbien@gmail.com>
2020-01-29 01:17:56 +0800

    refactor: t.Run()

--- 6-map/dictionary_test.go
+++ 6-map/dictionary_test.go
@@ -5,10 +5,12 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got := dictionary.Search("test")
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
