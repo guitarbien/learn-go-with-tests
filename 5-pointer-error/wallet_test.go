770f6e01dff2fa169c1cbd5aec5315c10a56607f
guitarbien <guitarbien@gmail.com>
2020-01-28 17:27:31 +0800

    refactor: 判斷何種錯誤，若該有 error 但為 nil 則跳 fatal

--- 5-pointer-error/wallet_test.go
+++ 5-pointer-error/wallet_test.go
@@ -11,9 +11,13 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
-	assertError := func(t *testing.T, err error) {
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
+	assertError := func(t *testing.T, got error, want string) {
+		if got == nil {
+			t.Fatal("wanted an error but didn't get one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got '%s', want '%s'", got, want)
 		}
 	}
 
@@ -35,6 +39,6 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, wallet, startingBalance)
-		assertError(t, err)
+		assertError(t, err, "cannot withdraw, insufficient funds")
 	})
 }
