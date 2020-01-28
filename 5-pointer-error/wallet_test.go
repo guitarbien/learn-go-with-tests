7550d31e824eb808cdd36f6a6c6cfbe7fad5d9a3
guitarbien <guitarbien@gmail.com>
2020-01-28 17:39:49 +0800

    test: 補上 no error 的 assertion

--- 5-pointer-error/wallet_test.go
+++ 5-pointer-error/wallet_test.go
@@ -11,8 +11,10 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
@@ -30,6 +32,12 @@ func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
 
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	if got != nil {
+		t.Fatal("got an error but didnt want one")
 	}
 }
 
