39813ecda8ef2cfb7156c9a3f518dcb25ed2b816
guitarbien <guitarbien@gmail.com>
2020-01-29 23:58:34 +0800

    feat: 使用 Update() 若 key 不存在應回傳錯誤

--- 6-map/dictionary.go
+++ 6-map/dictionary.go
@@ -5,6 +5,7 @@ type Dictionary map[string]string
 const (
 	ErrNotFound = DictionaryErr("could not find the word you were looking for")
 	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 type DictionaryErr string
@@ -39,6 +40,16 @@ func (d Dictionary) Add(word, definition string) error {
 }
 
 func (d Dictionary) Update(word, definition string) error {
-	d[word] = definition
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
 	return nil
 }
