b4f284d9461030ee6b2817ce35a64df6f81b27e1
guitarbien <guitarbien@gmail.com>
2020-01-29 23:25:17 +0800

    test: 新增不存在的 key 不會報錯

--- 6-map/dictionary_test.go
+++ 6-map/dictionary_test.go
@@ -20,13 +20,16 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
 
-	dictionary.Add(word, definition)
+		err := dictionary.Add(word, definition)
 
-	assertDefinition(t, dictionary, word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
