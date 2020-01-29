fbac69b2126036e6a9a1d0b93eb5f1718e9e9441
guitarbien <guitarbien@gmail.com>
2020-01-29 23:53:52 +0800

    feat: Update() 可以回傳錯誤

--- 6-map/dictionary.go
+++ 6-map/dictionary.go
@@ -38,6 +38,7 @@ func (d Dictionary) Add(word, definition string) error {
 	return nil
 }
 
-func (d Dictionary) Update(word, definition string) {
+func (d Dictionary) Update(word, definition string) error {
 	d[word] = definition
+	return nil
 }
