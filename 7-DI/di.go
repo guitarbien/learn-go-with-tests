eb0a51e34382473890e7c91cce340575e82e022e
guitarbien <guitarbien@gmail.com>
2020-02-02 00:56:57 +0800

    feat: http.ResponseWriter 也實作了 io.Writer

--- 7-DI/di.go
+++ 7-DI/di.go
@@ -8,3 +8,11 @@ import (
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
+
+// func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+// 	Greet(w, "world")
+// }
+//
+// func main() {
+// 	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
+// }
