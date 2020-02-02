2223c8d48c0d0cfa1cb954c8e7b8628961d28655
guitarbien <guitarbien@gmail.com>
2020-02-02 11:44:12 +0800

    test: 調整 want

--- 8-mocking/countdown_test.go
+++ 8-mocking/countdown_test.go
@@ -11,7 +11,10 @@ func TestCountdown(t *testing.T) {
 	Countdown(buffer)
 
 	got := buffer.String()
-	want := "3"
+	want := `3
+2
+1
+GO`
 
 	if got != want {
 		t.Errorf("got '%s' want '%s'", got, want)
