433b12d67038d34809159c82db3d5cbe3f096398
guitarbien <guitarbien@gmail.com>
2020-02-02 12:20:11 +0800

    refactor: use t.Run()

--- 8-mocking/countdown_test.go
+++ 8-mocking/countdown_test.go
@@ -6,22 +6,24 @@ import (
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &countdownOperationsSpy{}
+	t.Run("print 3 to GO", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &countdownOperationsSpy{}
 
-	Countdown(buffer, spySleeper)
+		Countdown(buffer, spySleeper)
 
-	got := buffer.String()
-	want := `3
+		got := buffer.String()
+		want := `3
 2
 1
 GO`
 
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
-	}
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
 
-	if len(spySleeper.Calls) != 4 {
-		t.Errorf("not enough calls to sleeper, want 4 got %d", len(spySleeper.Calls))
-	}
+		if len(spySleeper.Calls) != 4 {
+			t.Errorf("not enough calls to sleeper, want 4 got %d", len(spySleeper.Calls))
+		}
+	})
 }
