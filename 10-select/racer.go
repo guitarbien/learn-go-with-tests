11a575617fb6876572696d29e75763d2fa5dd4d8
guitarbien <guitarbien@gmail.com>
2020-02-15 16:01:49 +0800

    feat: 比較記錄兩個 url 的回應時間
    "net/http"
    "time"

--- 10-select/racer.go
+++ 10-select/racer.go
@@ -1,5 +1,22 @@
 package racer
 
+import (
+	"net/http"
+	"time"
+)
+
 func Racer(urlA, urlB string) (winner string) {
-	return winner
+	startA := time.Now()
+	http.Get(urlA)
+	urlADuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(urlB)
+	urlBDuration := time.Since(startB)
+
+	if urlADuration < urlBDuration {
+		return urlA
+	}
+
+	return urlB
 }
